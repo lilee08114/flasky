768c6ea1fd3378251a9d653607b9897854375d19
Lilee <yudamn2012@gmail.com>
2017-06-21 11:58:03 +0800

    manager profile

--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 from . import main
-from flask import render_template, flash
+from flask import render_template, flash, request,redirect,url_for
 from flask_login import login_required, current_user
 from ..forms import EditProfile
-from ..data_model import DBSession, Table1
+from ..data_model import DBSession, Table1, Permission
+from ..decorator import need_permission
 
 @main.route('/')
 def home_page():
@@ -19,9 +20,12 @@ def show_profile(username):
 @main.route('/edit_profile/', methods=['GET','POST'])
 @login_required
 def edit_profile():
+	'''
+	仍然有逻辑问题，为什么current_user不能直接用？
+	个人资料页面，应该对任意人开放，但编辑只对自己开放
 
+	'''
 	form = EditProfile()
-	print ('ssss',form.validate_on_submit())
 	if form.validate_on_submit():
 		
 		db_session = DBSession
@@ -37,7 +41,7 @@ def edit_profile():
 		flash('your profile has beem modified！')
 		db_session.close()
 		return render_template('edit_profile.html', form=form)
-	print ('pppppppppppp')	
+
 	form.realname.data = current_user.realname
 	form.gender.data = current_user.gender	 
 	form.age.data = current_user.age
@@ -45,3 +49,22 @@ def edit_profile():
 	form.introduction.data = current_user.introduction
 	return render_template('edit_profile.html', form=form)
 #------------------------------------------------------------------
+
+@main.route('/manager_system/',methods=['GET','POST'])
+@need_permission(Permission.FOLLOW|Permission.COMMENT|Permission.WRITE|Permission.SHUTDOWN)
+def manager_chart():
+	if request.method == 'POST':
+		db_session=DBSession
+		temp = db_session.query(Table1).filter_by(usermail=request.form['mail']).first()
+		if temp is None:
+			flash('this user is not exist!')
+			return render_template('main/manager_system.html')
+		else:
+			return redirect(url_for('main.manager_editor',user=temp))
+	return render_template('main/manager_system.html')
+
+
+@main.route('/edit_profile_manager/', methods=['GET','POST'])
+@need_permission(Permission.FOLLOW|Permission.COMMENT|Permission.WRITE|Permission.SHUTDOWN)
+def manager_editor(user):
+	pass
